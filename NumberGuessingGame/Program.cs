6fa7208c239e13572e81b8609e528c41c32376c4
Tony Easter <tony.easter@hotmail.co.uk>
2023-01-12 09:57:10 +0000

    First completed C# project

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,2 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+{
+    Random random = new Random();
+    bool playAgain = true;
+    int min = 1;
+    int max = 100;
+    int guess;
+    int number;
+    int guesses;
+    String response;
+
+    while(playAgain)
+    {
+        guess = 0;
+        guesses = 0;
+        response = "";
+        number = random.Next(min, max + 1);
+
+        while (guess != number)
+        {
+            Console.WriteLine("Guess a number between " + min + "-" + max + ": ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Guess: " + guess);
+
+            if (guess > number)
+            {
+                Console.WriteLine(guess + " is to high!");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine(guess + " is to low!");            
+            }
+            guesses++;
+            
+        }
+        Console.WriteLine("Number: " + number);
+        Console.WriteLine("YOU WIN!");
+        Console.WriteLine("It took you " + guesses + " guesses");
+
+        Console.WriteLine("Would you like to play agin? (Y/N): ");
+        response = Console.ReadLine();
+        response = response.ToUpper();
+
+        if (response == "Y")
+        {
+            playAgain = true;
+        }
+        else
+        {
+            playAgain= false;
+        }
+    }
+
+    Console.WriteLine("Thanks for playing ... I guess");
+
+    Console.ReadKey();
+}
